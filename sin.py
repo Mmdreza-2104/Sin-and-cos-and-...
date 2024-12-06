6186635cbab59a26448fb54290f22ad46a89dd3a
MRG2104 <mohammadrezagoudarzi756@gmail.com>
2024-12-06 18:57:59 +0330

    Add files via upload

---
+++ sin.py
@@ -0,0 +1,11 @@
+import mpmath
+
+num = int(input('Enter your number:'))
+
+print('sin:',mpmath.sin(mpmath.radians(num)))
+
+print('cos:',mpmath.cos(mpmath.radians(num)))
+
+print('tan:',mpmath.tan(mpmath.radians(num)))
+
+print('cot:',mpmath.cot(mpmath.radians(num)))
